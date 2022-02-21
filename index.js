80c4ce73beeabb18d2efcc46f056cb82a8522dc8
Angrymouse <rykov.nikas@gmail.com>
2022-02-21 02:52:37 +0200

    refactor&config

--- index.js
+++ index.js
@@ -1,40 +1,52 @@
-const fetchSiaZone = require('./fetch-sia-zone');
-const { wire: { types } } = require('bns');
-const { RecursiveServer } = require('hipr');
+const fetchSiaZone = require("./util/fetch-sia-zone");
+const {
+	wire: { types },
+} = require("bns");
+let config = require("json5").parse(
+	require("fs").readFileSync("./config.json5", "utf8")
+);
+const { RecursiveServer } = require("hipr");
 
-const root = '149.248.21.56:53'.split(':')
+const root = config.rootNameserver.split(":");
 
 const server = new RecursiveServer({
-  tcp: true,
-  inet6: true,
-  edns: false,
-  dnssec: false
-})
-server.on('log', console.log)
-server.parseOptions({ dnssec: true })
-server.resolver.setStub(root[0], parseInt(root[1]), require('./create-ds')())
+	tcp: true,
+	inet6: true,
+	edns: false,
+	dnssec: false,
+});
+server.on("log", console.log);
+server.parseOptions({ dnssec: true });
+server.resolver.setStub(
+	root[0],
+	parseInt(root[1]),
+	require("./util/create-ds")()
+);
 
 // HIP-5-ish
 
-const zones = new Map()
-server.resolver.use(':data.:protocol(_skyname|skyname.xyz).', async ({ protocol, data }, name, type) => {
-  data = data.split('.')
-  const skylink = data[data.length - 1]
-  try {
-    let zone = zones.get(skylink) 
+const zones = new Map();
+server.resolver.use(
+	":data.:protocol(_skyname|skyname.xyz).",
+	async ({ protocol, data }, name, type) => {
+		data = data.split(".");
+		const skylink = data[0];
+		try {
+			let zone = zones.get(skylink);
 
-    if (!zone) {
-      zone = await fetchSiaZone(skylink)
-      zones.set(skylink, zone)
-      console.log(zone)
-    }
-    const res = zone.resolve(name, types[type])
-    console.log(name, types[type], res)
-    return res
-  } catch (e) {
-    console.log('error', e)
-    return null
-  }
-})
+			if (!zone) {
+				zone = await fetchSiaZone(skylink);
+				zones.set(skylink, zone);
+				console.log(zone);
+			}
+			const res = zone.resolve(name, types[type]);
+			console.log(name, types[type], res);
+			return res;
+		} catch (e) {
+			console.log("error", e);
+			return null;
+		}
+	}
+);
 
-server.bind(5333, '127.0.0.1')
+server.bind(5333, "127.0.0.1");
---
+++ util/fetch-sia-zone.js
@@ -0,0 +1,44 @@
+const split2 = require("split2");
+const { Zone } = require("bns");
+let config = require("json5").parse(
+	require("fs").readFileSync("../config.json5", "utf8")
+);
+const { SkynetClient } = require("@skynetlabs/skynet-nodejs");
+const decode = require("./decodeSkylink");
+
+const client = new SkynetClient();
+
+module.exports = function (skylink, portalUrl = config.skynetPortal) {
+	skylink = decode(skylink);
+	return new Promise(async (resolve, reject) => {
+		const zone = new Zone();
+		try {
+			const response = await client.executeRequest({
+				portalUrl,
+				endpointPath: "/",
+				method: "get",
+				extraPath: skylink,
+				responseType: "stream",
+			});
+
+			const stream = response.data.pipe(split2());
+			stream.on("data", (data) => {
+				try {
+					if (data.length) {
+						data = data.toString();
+						zone.fromString(data);
+					}
+				} catch (error) {
+					stream.end(); // force end of stream if sia data cannot be parsed as zone file
+				}
+			});
+			stream.on("end", () => {
+				resolve(zone);
+			});
+			stream.on("error", console.error);
+		} catch (error) {
+			reject(error);
+		}
+	});
+	return zone;
+};
